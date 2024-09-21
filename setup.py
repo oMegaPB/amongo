c7879a609e47df6b468ffd9274430ad277246bc3
oMegaPB <gunter56741@gmail.com>
2024-09-22 01:06:48 +0300

    Update setup.py
    made package work

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     version="0.3.2",
     install_requires=["pymongo", "attrs"],
     packages=['amongo'],
+    description="small mongodb driver for local mongod",
+    author="oMegaPB",
+    url="https://github.com/oMegaPB/amongo",
 )
